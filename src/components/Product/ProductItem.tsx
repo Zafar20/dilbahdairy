e781df58f09bbb226091c7a3944f417e534d8fa8
Zafar20 <60092029+Zafar20@users.noreply.github.com>
2024-07-24 11:49:02 +0500

    Update ProductItem.tsx

--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -11,7 +11,7 @@ const ProductItem = ({ item, type }: { item: any, type: string }) => {
   return (
     <div className={s.product__item}>
       <div className={s.product__item_img}>
-        <Image  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill src={url} alt="" className={s.product__item_image} />
+        <Image  priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill src={url} alt="" className={s.product__item_image} />
       </div>
       <div className={s.product__item_info}>
         <h2 className={s.product__item_title}>{item.title}</h2>
